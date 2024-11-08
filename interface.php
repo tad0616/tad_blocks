392e4943bff64573bb24afea274d8767736dd554
tad0616 <tad0616@gmail.com>
2024-11-08 08:14:21 +0800

    修改版號

--- interface.php
+++ interface.php
@@ -1,11 +1,12 @@
 <?php
 //判斷是否對該模組有管理權限
 if (!isset($_SESSION['tad_blocks_adm'])) {
-    $_SESSION['tad_blocks_adm'] = ($xoopsUser) ? $xoopsUser->isAdmin() : false;
+    $_SESSION['tad_blocks_adm'] = isset($xoopsUser) && \is_object($xoopsUser) ? $xoopsUser->isAdmin() : false;
 }
 
 $interface_menu[_MD_TAD_BLOCKS_MY_BLOCKS] = "index.php";
 $interface_icon[_MD_TAD_BLOCKS_MY_BLOCKS] = "fa-cube";
+
 if ($_SESSION['tad_blocks_adm'] or $_SERVER['PHP_SELF'] == '/admin.php') {
     $interface_menu[_MD_TADBLOCKS_BLOCKS] = "blocks.php";
     $interface_icon[_MD_TADBLOCKS_BLOCKS] = "fa-cubes";
